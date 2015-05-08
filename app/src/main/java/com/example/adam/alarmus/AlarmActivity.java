6d2dd626edc1523c98bc9023979fca4c511860c6
Adam <adamkonenycz@gmail.com>
2015-05-08 21:50:26 +0200

    Initial commit

--- app/src/main/java/com/example/adam/alarmus/AlarmActivity.java
+++ app/src/main/java/com/example/adam/alarmus/AlarmActivity.java
@@ -26,6 +26,7 @@ public class AlarmActivity extends ActionBarActivity {
             public void onClick(View v) {
                 vibrator.cancel();
                 stopService(new Intent(getApplicationContext(), AlarmService.class));
+                /*sssssssssssssssss*/
             }
         });
     }
